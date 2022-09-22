af967c602e0a3f1afdac6803c1d7a727aad55b25
Abhireet Singh <sinabhir@amdocs.com>
2022-09-22 12:19:21 +0530

    New class file added in second commit

---
+++ src/main/java/com/learning/JenkinsIntegration.java
@@ -0,0 +1,10 @@
+package com.learning;
+
+public class JenkinsIntegration {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi I am learning Jenkins continous integration");
+	}
+
+}
